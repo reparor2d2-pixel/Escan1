02209f54e358a2a3363c2c4a41a02a93bd4e59f1
reparor2d2-pixel <reparor2d2@gmail.com>
2026-08-20 21:33:05 -0400

    Update google-config.js

--- google-config.js
+++ google-config.js
@@ -1,6 +1,6 @@
 window.EVALUACAM_CONFIG = {
   institutionalDomain: 'nsmquilpue.cl',
   adminEmail: 'c.cari@nsmquilpue.cl',
-  apiUrl: '',
-  googleClientId: 'PEGAR_ID_CLIENTE.apps.googleusercontent.com'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbzMK7oTmTvJEsy9vxBOOKhc1DzoHHm_BQM1njkiBHodT4fWl3dkVVO4mwp__pnJJ_QoMA/exec',
+  googleClientId: '926922434234-s1ganfv4lph6j248ae8m0o5ptd34psfk.apps.googleusercontent.com'
 };
